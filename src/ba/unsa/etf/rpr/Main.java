59451b0dc32a4f72568e7c1bda6512f87c5f23ec
mbobic1 <mbobic1@etf.unsa.ba>
2021-10-11 16:07:12 +0200

    Initial Commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 System.out.println("Test za git");
+        System.out.println("Test za git2");
+        System.out.println("Test za git3");
+    }
+}
